8bfe850d97fd458eb9759ca154d37adfb0953f58
shshrukh <shshrukh.arbaz@gmail.com>
2026-08-12 17:34:34 +0500

    Enhance user registration to include role and update user creation logic

--- src/dataBase/migrations/001.created_users.sql
+++ src/dataBase/migrations/001.created_users.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
     first_name VARCHAR(15) NOT NULL,
     last_name VARCHAR(15),
+    role VARCHAR(20) NOT NULL DEFAULT 'user',
     avatar TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -15,6 +15,14 @@ type RegisteredUser = {
     first_name: string;
     last_name: string;
     email: string;
+    role: string
+};
+
+type CreatedUser = {
+    id: number;
+    first_name: string;
+    last_name: string;
+    role: string;
 };
 
 
@@ -26,7 +34,7 @@ const registerUserService = async (payload: RegisterUserPayload): Promise<Regist
         [email]
     );
     console.log(existingUser);
-    
+
 
     if (existingUser.rowCount && existingUser.rowCount > 0) {
         throw new ConflictError("User with this email already exists");
@@ -37,18 +45,21 @@ const registerUserService = async (payload: RegisterUserPayload): Promise<Regist
     try {
         await client.query("BEGIN");
 
-        const userResult = await client.query<{ id: number }>(
-            "INSERT INTO users (first_name, last_name) VALUES ($1, $2) RETURNING id",
+        const userResult = await client.query<CreatedUser>(
+            "INSERT INTO users (first_name, last_name) VALUES ($1, $2) RETURNING id, first_name, last_name, role",
             [first_name, last_name]
         );
 
-        const userId = userResult.rows[0]?.id;
+        const user: CreatedUser | undefined = userResult.rows[0];
 
-        if (!userId) {
+        if (!user) {
             throw new InternalServerError("Unable to create user");
         }
 
-        const passwordHash =  await hashSecret(password);
+        const userId = user.id;
+        const role = user.role;
+
+        const passwordHash = await hashSecret(password);
 
         await client.query(
             "INSERT INTO auth (user_id, email, password_hash) VALUES ($1, $2, $3)",
@@ -62,6 +73,7 @@ const registerUserService = async (payload: RegisterUserPayload): Promise<Regist
             first_name,
             last_name,
             email,
+            role
         };
     } catch (error) {
         await client.query("ROLLBACK");

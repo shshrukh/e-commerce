ecca735772ad7af0eb6d3d3780b6f50ac65366cd
shshrukh <shshrukh.arbaz@gmail.com>
2026-08-21 22:29:19 +0500

    feat: add cloudinary image upload

---
+++ src/config/cloudinary.config.ts
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import type { cloudinaryConfig } from "../types/types.js";
+
+
+
+const cloudinaryConfigOptions: cloudinaryConfig = {
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+    api_key: process.env.CLOUDINARY_API_KEY!,
+    api_secret: process.env.CLOUDINARY_API_SECRET!
+}
+
+for(const [key, value] of Object.entries(cloudinaryConfigOptions)){
+    if(!value){
+        throw new Error(`Missing Cloudinary environment variable: ${key}`);
+    }
+}
+cloudinary.config({
+    ...cloudinaryConfigOptions,
+    secure: true
+});
+
+
+export {cloudinary};
+
+
+
+
---
+++ src/utils/uploadImage.ts
@@ -0,0 +1,5 @@
+import { cloudinary } from "../config/cloudinary.config.js"
+
+const uploadImageOnCloudinary = async (image: string)=>{
+    cloudinary
+}
